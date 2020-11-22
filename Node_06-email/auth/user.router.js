4bcd72a621c0208fd9d50c0c1c3bccbf1c01a865
Mac <mac@MacBook-Pro-Mac-2.local>
2020-11-22 16:11:11 +0200

    add

---
+++ Node_06-email/auth/user.router.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const userRouter = express.Router();
+const UserController = require("./user.controller");
+
+const user = new UserController();
+
+userRouter.post("/register", user.createUser);
+userRouter.post("/login", user.logIn);
+userRouter.post("/logout", user.authorize, user.logOut);
+userRouter.get("/current", user.authorize, user.getCurrentUser);
+userRouter.get("/verify/:verificationToken", user.verifyUser);
+
+module.exports = userRouter;
